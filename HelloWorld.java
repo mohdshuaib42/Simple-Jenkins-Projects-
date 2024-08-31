9ff1819f2f219fc3c1c2b5082d5c52543eae6406
Mohd Shuaib <mohdshuaib0142@gmail.com>
2024-08-31 13:29:44 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
 	    public static void main(String[] args) {
 		            System.out.println("Hello, World, this is Simple!");
+		            System.out.println(22+20 + " is the number!");
+		         
 			        }
 }
-
